57ccfe369e19e59534c3759fd974121c3b78d1de
Christian Ost <christian@christianost.de>
2020-01-21 21:33:03 +0100

    fix: add empty build script, use correct pathes for ssh key

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ set -eo pipefail
 echo $SERVER_PUBLIC_KEY >> $HOME/.ssh/known_hosts
 
 # copy compose file to server
-rsync --progress -e "ssh -i $HOME/.ssh/der-nackte-halloumi-base-travis" docker-compose.yml $SERVER_ADDRESS:.
+rsync --progress -e "ssh -i der-nackte-halloumi-base-travis" docker-compose.yml $SERVER_ADDRESS:.
 
 # restart services
-ssh -i $HOME/.ssh/der-nackte-halloumi-base-travis $SERVER_ADDRESS "docker-compose up --build -d"
+ssh -i der-nackte-halloumi-base-travis $SERVER_ADDRESS "docker-compose up --build -d"
