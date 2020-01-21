bca3237205532cbfeef23ff2b6fbf9b83a07940a
Christian Ost <christian@christianost.de>
2020-01-21 07:50:15 +0100

    feat: add deployment and setup scripts

---
+++ scripts/setup-machine.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+apt remove docker docker-engine docker.io containerd runc
+
+apt update
+apt upgrade -y
+
+apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+apt update
+apt install docker-ce docker-ce-cli containerd.io -y
+
+
+curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
