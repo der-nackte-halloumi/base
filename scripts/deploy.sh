14c176cfc46f47fd556283cfff10b2435809b51b
Christian Ost <christian@christianost.de>
2020-01-23 22:11:49 +0100

    fix: pull images before restarting them

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,4 +10,4 @@ echo ">>> transfer docker-compose.yml to server"
 rsync --progress -e "ssh -i der-nackte-halloumi-base-travis" docker-compose.yml $SERVER_ADDRESS:.
 
 echo ">>> restart services to apply new config"
-ssh -i der-nackte-halloumi-base-travis $SERVER_ADDRESS "docker-compose up --build -d"
+ssh -i der-nackte-halloumi-base-travis $SERVER_ADDRESS "docker-compose pull && docker-compose up --build -d"
