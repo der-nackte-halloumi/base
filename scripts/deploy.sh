3e6f8aaaffe1135478ea3635ed669522d205dead
Christian Ost <hallo@christianost.de>
2020-02-20 16:48:08 +0100

    chore: update logs to have something changed

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,5 +9,5 @@ echo $SERVER_PUBLIC_KEY >> $HOME/.ssh/known_hosts
 echo ">>> transfer docker-compose.yml to server"
 rsync --progress -e "ssh -i der-nackte-halloumi-base-travis" docker-compose.yml $SERVER_ADDRESS:.
 
-echo ">>> restart services to apply new config"
+echo ">>> restart all services to apply new config"
 ssh -i der-nackte-halloumi-base-travis $SERVER_ADDRESS "docker-compose pull && docker-compose up --build -d"
